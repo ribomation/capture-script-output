c3aa18523759057f1806a11665e68b9215454df0
Jens Riboe <jens.riboe@ribomation.se>
2024-04-02 13:02:50 +0200

    update

--- vars/runScript.groovy
+++ vars/runScript.groovy
@@ -1,19 +1,18 @@
 
 @NonCPS
 String patch(String template, Map context) {
-    //https://stackoverflow.com/questions/42295921/what-is-the-effect-of-noncps-in-a-jenkins-pipeline-script
-    //https://stackoverflow.com/questions/59928804/groovy-vs-jenkins-regex-replacefirst-replaceall-function-with-closure-works-di
-    def pattern = /\@(\w+)\@/
+    String pattern      = /@(\w+)@/
     Closure replacement = {_,key -> context[key]}
     String scriptText   = template.replaceAll(pattern, replacement) 
     return scriptText
 }
 
 def call(Map params = [:]) {
-    def name = params.name ?: 'run-script'
-    def cmd  = params.cmd  ?: 'ls -lhFA .'
-
-    def tmpl = '''
+    def context  = [
+        NAME: params.name ?: 'run-script', 
+        CMD : params.cmd  ?: 'ls -lhFA .'
+        ]
+    def template = '''
         #!/usr/bin/env bash
         set -e            #stop at first error (non-zero exit code)
         set -u            #stop if using undef variable
@@ -26,28 +25,18 @@ def call(Map params = [:]) {
         echo "script   : @NAME@"
         @CMD@
         '''.stripIndent().trim()
-
-    def ctx  = [NAME:name, CMD:cmd]
-    // echo "ctx: ${ctx}"
-
-    def scriptText = patch(tmpl, ctx)
-    // echo '--script--'
-    // echo scriptText
-    // echo '--end script--'
+    def scriptText = patch(template, context)
     
     def dir = "./TMP.${name}.${System.nanoTime()}"
-    writeFile file:"${dir}/cmd.sh", text:scriptText
+    writeFile file:"${dir}/cmd.sh",     text:scriptText
     writeFile file:"${dir}/stdout.txt", text:''
     writeFile file:"${dir}/stderr.txt", text:''
-    writeFile file:"${dir}/exit.txt", text:''
-    writeFile file:"${dir}/README.txt", text:'Temp-dir created for invocation of runScript'
+    writeFile file:"${dir}/exit.txt",   text:''
+    writeFile file:"${dir}/README.txt", text:'Temp-dir for runScript'
     
     sh "chmod a+x ${dir}/cmd.sh"
-    // sh "ls -lhFA ${dir}"
-    
-    def SH_CMD = "${dir}/cmd.sh 2>${dir}/stderr.txt 1>${dir}/stdout.txt; echo \$? >${dir}/exit.txt"
-    // echo 'SH_CMD: "' + SH_CMD + '"'
-    sh SH_CMD
+    def cmdLine = "${dir}/cmd.sh 2>${dir}/stderr.txt 1>${dir}/stdout.txt; echo \$? >${dir}/exit.txt"
+    sh cmdLine
 
     def result  = [:]
     result.out  = readFile "${dir}/stdout.txt"
